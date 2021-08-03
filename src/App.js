a72e1032de7a516911c111a86be4290e14620070
Oleksii Protasov <oleksi.protasov@gmail.com>
2021-08-03 15:50:58 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import moduleName from './Components/input/';
-import { Component } from "react";
+// import { Component } from "react";
+import { useState, useEffect } from "react";
 import shortid from "shortid";
 
 import Container from "./Components/Container";
@@ -7,8 +7,8 @@ import Form from "./Components/form";
 import Filter from "./Components/filter";
 import ContactList from "./Components/Phonebook";
 
-class App extends Component {
-  state = {
+export default function App() {
+  const state = {
     contacts: [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
@@ -18,7 +18,7 @@ class App extends Component {
     filter: "",
   };
 
-  addContact = (task) => {
+  const addContact = (task) => {
     const searchSameName = this.state.contacts
       .map((cont) => cont.name)
       .includes(task.name);
@@ -39,11 +39,11 @@ class App extends Component {
     }
   };
 
-  changeFilter = (filter) => {
+  const changeFilter = (filter) => {
     this.setState({ filter });
   };
 
-  getVisibleContacts = () => {
+  const getVisibleContacts = () => {
     const { contacts, filter } = this.state;
 
     return contacts.filter((contacts) =>
@@ -51,7 +51,7 @@ class App extends Component {
     );
   };
 
-  removeContact = (contactId) => {
+  const removeContact = (contactId) => {
     this.setState((prevState) => {
       return {
         contacts: prevState.contacts.filter(({ id }) => id !== contactId),
@@ -59,42 +59,130 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
-    const contact = localStorage.getItem("contacts");
-    const parsetContact = JSON.parse(contact);
+  // componentDidMount() {
+  //   const contact = localStorage.getItem("contacts");
+  //   const parsetContact = JSON.parse(contact);
 
-    if (parsetContact) {
-      this.setState({ contacts: parsetContact });
-    }
-  }
+  //   if (parsetContact) {
+  //     this.setState({ contacts: parsetContact });
+  //   }
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevStates = this.state.contacts;
+  // componentDidUpdate(prevProps, prevState) {
+  //   const prevStates = this.state.contacts;
 
-    if (prevStates !== prevState) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
+  //   if (prevStates !== prevState) {
+  //     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  //   }
+  // }
 
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
+  const { filter } = state;
+  const visibleContacts = getVisibleContacts();
 
-    return (
-      <>
-        <Container>
-          <h1>Phonebook</h1>
-          <Form onAddContact={this.addContact} />
-          <h2>Contacts</h2>
-          <Filter value={filter} onChangeFilter={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            onRemoveContact={this.removeContact}
-          />
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container>
+        <h1>Phonebook</h1>
+        <Form onAddContact={addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChangeFilter={changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onRemoveContact={removeContact}
+        />
+      </Container>
+    </>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//     ],
+//     filter: "",
+//   };
+
+//   addContact = (task) => {
+//     const searchSameName = this.state.contacts
+//       .map((cont) => cont.name)
+//       .includes(task.name);
+
+//     if (searchSameName) {
+//       alert(`${task.name} is already in contacts`);
+//     } else if (task.name.length === 0) {
+//       alert("Fields must be filled!");
+//     } else {
+//       const contact = {
+//         ...task,
+//         id: shortid.generate(),
+//       };
+
+//       this.setState((prevState) => ({
+//         contacts: [...prevState.contacts, contact],
+//       }));
+//     }
+//   };
+
+//   changeFilter = (filter) => {
+//     this.setState({ filter });
+//   };
+
+//   getVisibleContacts = () => {
+//     const { contacts, filter } = this.state;
+
+//     return contacts.filter((contacts) =>
+//       contacts.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+//   };
+
+//   removeContact = (contactId) => {
+//     this.setState((prevState) => {
+//       return {
+//         contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+//       };
+//     });
+//   };
+
+//   componentDidMount() {
+//     const contact = localStorage.getItem("contacts");
+//     const parsetContact = JSON.parse(contact);
+
+//     if (parsetContact) {
+//       this.setState({ contacts: parsetContact });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevStates = this.state.contacts;
+
+//     if (prevStates !== prevState) {
+//       localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   render() {
+//     const { filter } = this.state;
+//     const visibleContacts = this.getVisibleContacts();
+
+//     return (
+//       <>
+//         <Container>
+//           <h1>Phonebook</h1>
+//           <Form onAddContact={this.addContact} />
+//           <h2>Contacts</h2>
+//           <Filter value={filter} onChangeFilter={this.changeFilter} />
+//           <ContactList
+//             contacts={visibleContacts}
+//             onRemoveContact={this.removeContact}
+//           />
+//         </Container>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Components/form/form.js
+++ src/Components/form/form.js
@@ -1,6 +1,82 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styles from "./style.module.css";
+// import {useState, useEffect} from 'react'
+
+// const useRecording = (key, defaultValue) => {
+//   const [state,setState] = useState(() => {
+//     return
+//   })
+// }
+
+// export default function Form() {
+//   const [name, setName] = useRecording('name', '');
+//   const [number, setNamber] = useRecording('number', '');
+//   // state = {
+//   //   name: "",
+//   //   number: "",
+//   // };
+
+// const handleChange = e => {
+//   const {name, value } = e.target;
+
+//   switch (name){
+//     case 'name': setName(value);
+//     break;
+//     case 'namber': setNamber(value);
+//     break;
+//     default:return;
+//   }
+// }
+
+//   // const handleChange = (e) => {
+//   //   const { name, value } = e.target;
+//   //   this.setState({
+//   //     [name]: value,
+//   //   });
+//   // };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     this.props.onAddContact({ ...this.state });
+
+//     this.setState({ name: "", number: "" });
+//   };
+
+//     return (
+//       <form className={styles.TaskEditor} onSubmit={handleSubmit}>
+//         <label className={styles.TaskEditor_label}>
+//           Name
+//           <input
+//             className={styles.TaskEditor_input}
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label className={styles.TaskEditor_label}>
+//           Number
+//           <input
+//             className={styles.TaskEditor_input}
+//             type="text"
+//             name="number"
+//             value={number}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <button className={styles.TaskEditor_button} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+
+// }
+
+// Form.propTypes = {
+//   onAddContact: PropTypes.func.isRequired,
+// };
 
 export default class Form extends Component {
   state = {
